0988bbd000ce1a020cd31fa8582bab0878166431
216351ks <kacpersiwiec00@interia.pl>
2020-05-16 18:23:49 +0200

    KS 2020-05-16

--- analyzer.py
+++ analyzer.py
@@ -15,10 +15,38 @@ product_id = input("Podaj identyfikator produktu: ")
 opinions = pd.read_json(input_directory + "/" + product_id + ".json")
 opinions = opinions.set_index("opinion_id")
 
+# podstawowe statystyki
 average_score = opinions.stars.mean().round(2)
 pros = opinions.pros.count()
 cons = opinions.cons.count()
+stars = opinions.stars.value_counts().sort_index().reindex(
+    list(np.arange(0, 5.5, 0.5)), fill_value=0)
 
-stars = opinions.stars.value_counts()
 
-print(stars, cons, pros)
+print(
+    f"Srednia ocena: {average_score}\nLiczba opinii z zaletami: {pros}\nLiczba opinii z wadami: {cons}\n{stars}")
+
+# histogram częstości występowania poszczególnych ocen (gwiazdek)
+recommendation = opinions.recommendation.value_counts()
+fig, ax = plt.subplots()
+stars.plot.bar(color="#f5c3c2")
+plt.title("***Gwiazdki***")
+plt.xlabel("liczba gwiazdek")
+plt.ylabel("Liczba opinii")
+plt.xticks(rotation=0)
+# plt.show()
+plt.savefig("figures_png/" + product_id+"_bar.png")
+plt.close()
+
+
+# wykres ile osób polaca a ile nie poleca
+fig, ax = plt.subplots()
+recommendation.plot.pie(label="", autopct="%1.1f%%",
+                        colors=["#89cff0", "#f5c3c2"])
+plt.title("REKOMENDACJA")
+# plt.show()
+plt.savefig("figures_png/" + product_id+"_bar.png")
+plt.close()
+
+opinions["purchased"] = opinions.purchase_date != None
+stars_purchased = pd.crosstab(opinions["stars"], opinions["purchased"])
